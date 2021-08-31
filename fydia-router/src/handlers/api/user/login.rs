d80b2113b7a29540a9db608f6f509bf82f9abd29
Rheydskey <matgag02@gmail.com>
2021-08-31 22:27:57 +0200

    Fydia-router: Rewrite login function

--- fydia-router/src/handlers/api/user/login.rs
+++ fydia-router/src/handlers/api/user/login.rs
@@ -23,50 +23,42 @@ pub async fn user_login(mut state: State) -> HandlerResult {
 
         return Ok((state, res));
     }
-    let from_json =
-        serde_json::from_str::<value::Value>(String::from_utf8(body).unwrap().as_str()).unwrap();
-    let email = if let Some(name) = from_json.get("email") {
-        if let Some(e) = name.as_str() {
-            e.to_string()
-        } else {
-            *res.status_mut() = StatusCode::BAD_REQUEST;
-            *res.body_mut() = "Error on email".into();
 
-            return Ok((state, res));
+    if let Ok(json) =
+        serde_json::from_str::<value::Value>(String::from_utf8(body).unwrap().as_str())
+    {
+        match (json.get("email"), json.get("password")) {
+            (Some(email), Some(password)) => match (email.as_str(), password.as_str()) {
+                (Some(email), Some(password)) => {
+                    let user = User::get_user_by_email_and_password(
+                        email.to_string(),
+                        password.to_string(),
+                        database,
+                    )
+                    .await;
+
+                    match user {
+                        Some(user) => {
+                            let token = user.unwrap().update_token(database).await.unwrap();
+                            *res.body_mut() = token.into();
+                        }
+                        None => {
+                            *res.status_mut() = StatusCode::BAD_REQUEST;
+                            *res.body_mut() = "User not exists".into();
+                        }
+                    }
+                }
+                _ => {
+                    *res.status_mut() = StatusCode::BAD_REQUEST;
+                    *res.body_mut() = "Error on json".into();
+                }
+            },
+            _ => {
+                *res.status_mut() = StatusCode::BAD_REQUEST;
+                *res.body_mut() = "Error on json".into();
+            }
         }
-    } else {
-        *res.status_mut() = StatusCode::BAD_REQUEST;
-        *res.body_mut() = "Error on email".into();
-
-        return Ok((state, res));
-    };
-    let password = if let Some(name) = from_json.get("password") {
-        if let Some(e) = name.as_str() {
-            e.to_string()
-        } else {
-            *res.status_mut() = StatusCode::BAD_REQUEST;
-            *res.body_mut() = "Error on password".into();
-
-            return Ok((state, res));
-        }
-    } else {
-        *res.status_mut() = StatusCode::BAD_REQUEST;
-        *res.body_mut() = "Error on password".into();
-
-        return Ok((state, res));
-    };
-
-    let user = User::get_user_by_email_and_password(email, password, database).await;
-
-    if user.is_none() {
-        *res.status_mut() = StatusCode::BAD_REQUEST;
-        *res.body_mut() = "User not exists".into();
-
-        return Ok((state, res));
     }
-    let token = user.unwrap().update_token(database).await.unwrap();
-
-    *res.body_mut() = token.into();
 
     Ok((state, res))
 }
