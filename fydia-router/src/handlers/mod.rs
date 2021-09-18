50d76e5af3d53c55b55b10de08499b7b3b275279
Rheydskey <matgag02@gmail.com>
2021-09-18 17:34:18 +0200

    fydia-router: Clean up

--- fydia-router/src/handlers/mod.rs
+++ fydia-router/src/handlers/mod.rs
@@ -3,7 +3,6 @@ use gotham::state::State;
 pub mod api;
 pub mod event;
 pub mod federation;
-pub mod json;
 
 pub fn default(state: State) -> (State, String) {
     let e = "Default. This request will be implemented soon".to_string();
