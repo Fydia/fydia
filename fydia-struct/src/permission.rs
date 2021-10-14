bed71e2798c0b1f7ccf4cd722112f3203b840dd6
Rheydskey <matgag02@gmail.com>
2021-10-14 21:33:29 +0200

    Fydia-struct: (WIP) Permissions

--- fydia-struct/src/permission.rs
+++ fydia-struct/src/permission.rs
@@ -4,27 +4,35 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Deserialize, Serialize, Debug)]
 pub enum Permission {
-    Write = 2,
-    Read = 1,
-    None = 0,
+    Admin = (1 << 2),
+    Write = (1 << 1),
+    Read = (1 << 0),
+    NoPerm = 0,
 }
 
 impl Permission {
-    pub fn from_string(from: String) -> Self {
+    pub fn from_string<T: Into<String>>(from: T) -> Self {
+        let from: String = from.into();
         match from.to_ascii_uppercase().as_str() {
             "WRITE" => Permission::Write,
             "READ" => Permission::Read,
-            _ => Permission::None,
+            _ => Permission::NoPerm,
         }
+    }
+
+    pub fn can(perms: u32, perm: Permission) -> bool {
+        let perm_as_u32 = perm as u32;
+        perms & perm_as_u32 == perm_as_u32
     }
 }
 
 impl Display for Permission {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
+            Permission::Admin => write!(f, "ADMIN"),
             Permission::Write => write!(f, "WRITE"),
             Permission::Read => write!(f, "READ"),
-            Permission::None => write!(f, "NONE"),
+            Permission::NoPerm => write!(f, "NOPERM"),
         }
     }
 }
