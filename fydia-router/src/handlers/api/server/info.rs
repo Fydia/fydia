7588e4c6c3c529d1254fdb6af6adacaa545a0455
Rheydskey <matgag02@gmail.com>
2021-11-22 19:40:34 +0100

    Fydia-router: Fix /api/server handler

--- fydia-router/src/handlers/api/server/info.rs
+++ fydia-router/src/handlers/api/server/info.rs
@@ -1,21 +1,19 @@
-use axum::body::Body;
 use axum::extract::Extension;
-use axum::http::Request;
 use axum::response::IntoResponse;
 use fydia_sql::impls::user::SqlUser;
 use fydia_sql::sqlpool::DbConnection;
 use fydia_struct::response::FydiaResponse;
 use fydia_struct::user::{Token, User};
+use http::HeaderMap;
 
 use crate::new_response;
 
 pub async fn get_server_of_user(
-    request: Request<Body>,
+    headers: HeaderMap,
     Extension(database): Extension<DbConnection>,
 ) -> impl IntoResponse {
-    let headers = request.headers();
     let mut res = new_response();
-    let token = if let Some(token) = Token::from_headervalue(headers) {
+    let token = if let Some(token) = Token::from_headervalue(&headers) {
         token
     } else {
         FydiaResponse::new_error("Bad Token").update_response(&mut res);
