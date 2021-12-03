cbb4cb9a90062ecd23c307aeeb80b7261236fb6a
Rheydskey <matgag02@gmail.com>
2021-12-03 07:08:30 +0100

    Fydia: Get Ip of Server and change domain if not set

--- fydia/src/main.rs
+++ fydia/src/main.rs
@@ -6,7 +6,23 @@ use fydia_config::get_config_or_init;
 /// Start function
 #[tokio::main]
 async fn main() {
-    let config = get_config_or_init();
+    let mut config = get_config_or_init();
+    if config.instance.domain.is_empty() {
+        if let Ok(req) = reqwest::Client::new()
+            .get("http://ifconfig.io")
+            .header("User-Agent", "curl/7.55.1")
+            .send()
+            .await
+        {
+            if let Ok(text) = req.text().await {
+                config.instance.domain = text;
+            } else {
+                panic!("Domain is not valid")
+            }
+        } else {
+            panic!("Domain is not valid")
+        }
+    };
     axum::Server::bind(&(config.format_ip().as_str()).parse().unwrap())
         .serve(
             fydia_router::get_axum_router(config)
