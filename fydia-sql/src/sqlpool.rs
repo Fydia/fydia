38c960b48d3b133352ab28127528d50d1c1cf97f
Rheydskey <matgag02@gmail.com>
2022-01-23 12:55:16 +0100

    fydia-{sql, struct}: Remove parse_array

--- fydia-sql/src/sqlpool.rs
+++ fydia-sql/src/sqlpool.rs
@@ -2,25 +2,3 @@ use sea_orm::DatabaseConnection;
 use std::sync::Arc;
 
 pub type DbConnection = Arc<DatabaseConnection>;
-
-pub fn parse_array(parse: String) -> Vec<String> {
-    let mut result = Vec::new();
-
-    let split = if let Some(prefix_stripped) = parse.strip_prefix('[') {
-        if let Some(suffix_stripped) = prefix_stripped.strip_suffix(']') {
-            suffix_stripped.split(',')
-        } else {
-            return Vec::new();
-        }
-    } else {
-        return Vec::new();
-    };
-
-    for i in split {
-        let striped = i.replace('"', "").replace(" ", "");
-
-        result.push(striped.to_string())
-    }
-
-    result
-}
--- fydia-struct/src/roles.rs
+++ fydia-struct/src/roles.rs
@@ -8,6 +8,14 @@ pub struct Role {
     pub server_id: ServerId,
     pub name: String,
     pub color: String,
-    pub channel_access: Vec<String>,
+    pub channel_access: ChannelAccess,
     pub permission: Permission,
+}
+#[derive(Deserialize, Serialize, Debug)]
+pub struct ChannelAccess(pub Vec<String>);
+
+impl ChannelAccess {
+    pub fn to_string(&self) -> Result<String, serde_json::Error> {
+        serde_json::to_string(&self)
+    }
 }
