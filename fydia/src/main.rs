cba802fcc790bb7b58bdaacd0f499164aa1fb598
Rheydskey <matgag02@gmail.com>
2022-02-08 00:39:27 +0100

    fydia: Rewrite start function

--- fydia/src/main.rs
+++ fydia/src/main.rs
@@ -5,24 +5,26 @@ use fydia_config::get_config_or_init;
 
 /// Start function
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), ()> {
     let mut config = get_config_or_init();
     if config.instance.domain.is_empty() {
-        if let Ok(req) = reqwest::Client::new()
+        let req = reqwest::Client::new()
             .get("http://ifconfig.io")
             .header("User-Agent", "curl/7.55.1")
             .send()
             .await
-        {
-            if let Ok(text) = req.text().await {
-                config.instance.domain = text;
-            } else {
-                panic!("Domain is not valid")
-            }
-        } else {
-            panic!("Domain is not valid")
-        }
+            .map_err(|_| {
+                panic!("Domain is not valid");
+            })?;
+
+        let text = req
+            .text()
+            .await
+            .map_err(|_| panic!("Domain is not valid"))?;
+
+        config.instance.domain = text;
     };
+
     axum::Server::bind(&(config.format_ip().as_str()).parse().unwrap())
         .serve(
             fydia_router::get_axum_router_from_config(config)
@@ -30,5 +32,7 @@ async fn main() {
                 .into_make_service(),
         )
         .await
-        .unwrap()
+        .unwrap();
+
+    Ok(())
 }
