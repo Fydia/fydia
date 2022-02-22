dc386a98c082019837439e33fc1a87a70ea8d697
Rheydskey <matgag02@gmail.com>
2022-02-22 12:59:57 +0100

    fydia-crypto: Increase lenght of key

--- fydia-crypto/src/key/generate.rs
+++ fydia-crypto/src/key/generate.rs
@@ -1,5 +1,7 @@
 use openssl::{error::ErrorStack, pkey::Private, rsa::Rsa};
 
+const LENGHT: u32 = 4096;
+
 pub fn generate_key() -> Result<Rsa<Private>, ErrorStack> {
-    Rsa::generate(2048)
+    Rsa::generate(LENGHT)
 }
