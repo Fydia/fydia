e3d0cf6343e9fb2657ab3ff9b41d776e6b6cef1a
Rheydskey <matgag02@gmail.com>
2022-03-08 22:14:49 +0100

    fydia-struct: Remove lenght requirement on user

--- fydia-struct/src/user.rs
+++ fydia-struct/src/user.rs
@@ -50,18 +50,6 @@ impl User {
             return Err("Password is empty".to_string());
         }
 
-        if name.len() < 5 {
-            return Err("Name is too short".to_string());
-        }
-
-        if email.len() < 5 {
-            return Err("Email is too short".to_string());
-        }
-
-        if password.len() < 5 {
-            return Err("Password is too short".to_string());
-        }
-
         Ok(User {
             name,
             instance,
